bcec02da696cd7ed32b53c857d9db5b3b1e69da1
Michael Hearn <git@mikehearn.net>
2018-03-20 08:27:15 -0400

    fuzzy matching

--- rsvp_backend/rsvp/views.py
+++ rsvp_backend/rsvp/views.py
@@ -1,4 +1,6 @@
+from fuzzywuzzy import process
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from rsvp.models import Invitation, Person
 from rsvp.serializers import InvitationSerializer, PersonSerializer
@@ -8,6 +10,26 @@ class PersonViewSet(viewsets.ModelViewSet):
     resource_name = 'people'
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+    def list(self, request, *args, **kwargs):
+        if request.query_params.get('filter[fullName]'):
+            name_filter = request.query_params.get('filter[fullName]')
+            people_dict = {p.__str__(): p.id for p in Person.objects.all() if not p.is_unknown_guest}
+            people = list(people_dict)
+            name_match = process.extract(name_filter, people)[0][0]
+            queryset = Person.objects.filter(id=people_dict[name_match])
+
+        if not queryset:
+            queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 
 class InvitationViewSet(viewsets.ModelViewSet):
